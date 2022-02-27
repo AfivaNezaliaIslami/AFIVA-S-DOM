33f2e7ef860bea747bdb2a654febc54ef5433fe5
AfivaNezaliaIslami <100521489+AfivaNezaliaIslami@users.noreply.github.com>
2022-02-27 22:01:20 +0700

    Create size().java

---
+++ Tugas 1 SDA/size().java
@@ -0,0 +1,19 @@
+package tugasSDA1;
+
+import java.util.ArrayList;
+public class sizeTest {
+     public static void main(String[] args) {
+          ArrayList nama = new ArrayList();
+
+          nama.add("L");
+          nama.add("A");
+          nama.add("M");
+          nama.add("I");
+
+         System.out.println("//Mengetahui panjang array");
+         System.out.println(nama);
+         System.out.println("Panjang array nama = "+nama.size());
+        
+    }
+    
+}
